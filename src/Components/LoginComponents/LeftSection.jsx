2c384a2a4912f306a74b2088da363b3ae6e752f2
AjmalNoordheen <noorudheenajmal@gmail.com>
2024-01-23 19:59:45 +0530

    made the login component responcive

--- src/Components/LoginComponents/LeftSection.jsx
+++ src/Components/LoginComponents/LeftSection.jsx
@@ -34,14 +34,14 @@ function LeftSection() {
   
   return (
     <>
-        <div className='flex flex-col justify-around items-center w-[50%] h-full'>
+        <div className='flex flex-col justify-around items-center w-full sm:w-[50%] h-[50%] sm:h-full'>
             <Logo/>  {/* Top Logo div */}
            {/* Login Section */} 
-          <div className='h-[70%] w-10/12 flex justify-center'> 
-            <div className='flex flex-col items-center justify-evenly h-[60%] w-8/12 mt-2'>
-              <div className='flex flex-col justify-center gap-y-3 items-center'>
+          <div className='h-[100%] sm:h-[70%] w-full sm:w-10/12 flex justify-center'> 
+            <div className='flex flex-col items-center space-y-10 sm:space-y-0 justify-center sm:justify-evenly h-[80%] md:h-[60%] w-8/12 mt-2'>
+              <div className='flex flex-col justify-center gap-y-3  items-center'>
                 <h1 className='font-medium text-lg'>LOGIN</h1>
-                <p className='font-normal text-[#0000008c] text-center text-sm leading-[20px]'>
+                <p className='font-normal text-[#0000008c] text-center text-xs sm:text-sm lg:text-base xl:text-sm leading-[15px]  sm:leading-[20px]'>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquet at eleifend feugiat vitae
                   faucibus nibh dolor dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquet
                   at eleifend feugiat vitae faucibus nibh dolor dui.
@@ -49,12 +49,12 @@ function LeftSection() {
               </div>
 
               {/* Google Sign-In Button */}
-              <div onClick={signUp} className='w-8/12 h-9 bg-[#597EF7] cursor-pointer px-0.5 flex justify-center items-center'>
+              <div onClick={signUp} className=' w-full sm:w-10/12 xl:w-8/12  h-9 lg:h-12 xl:h-9 bg-[#597EF7] cursor-pointer px-0.5 flex justify-center items-center'>
                 <div className='bg-white h-[87%] w-2/12 rounded-sm flex justify-center items-center'>
                   <img className='h-6' src="5847f9cbcef1014c0b5e48c8.png" alt="" />
                 </div>
                 <div className='w-10/12 text-center'>
-                  <p className='text-base text-white'>Sign in using Google</p>
+                  <p className=' text-sm lg:text-base text-white'>Sign in using Google</p>
                 </div>
               </div>
             </div>
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -4,7 +4,7 @@ import RightSection from '../Components/LoginComponents/RightSection'
 
 function Login() {
   return (
-    <div className='w-screen h-screen bg-white flex'>
+    <div className='w-screen h-screen bg-white sm:flex '>
         <LeftSection/>
         <RightSection/>
     </div>
